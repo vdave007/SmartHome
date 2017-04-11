2af85017659ba0c0e1b2b5f7e764a88eca0f8371
Zsebe Akos <zsebea@yahoo.com>
2017-04-11 12:40:24 +0000

    notification sender

--- backend/socket.js
+++ backend/socket.js
@@ -1,21 +1,47 @@
 'use strict'
 
+var sockets = [],
+    user_data = []
+
+// --- ez majd nem igy kell ---   
+var request = require("request");
+//  --------------------------
+
 
 module.exports = (io) => {
 
     var socketnumber = 0;
     
     io.on('connection', function(socket){
-        console.log('connected', ++socketnumber);
+        console.log('connected');
         
-        socket.on('chat message', function(msg){
-            console.log('chat message', msg);
+        
+        socket.on('register', function(msg){
+            msg = JSON.parse(msg)
+            sockets.push(socket)
+            msg.id += socketnumber
+            socketnumber++
+            user_data.push(msg)
+            console.log('new user', msg.id +" --- "+ msg.house_id);
         });
         
         socket.on('disconnect', function () {
             socketnumber--
+            delete sockets[socketnumber]
             console.log('diconnected', socketnumber);
         });
+       
           
     });
+    
+    setInterval(function(){
+        var index = 0;
+        sockets.forEach(function(s){
+            request("https://allamvizsga-akoszsebe.c9users.io/getdeviceswithsettings?house_id="+user_data[index].house_id, function(error, response, body) {
+                console.log(body);
+                s.emit('notification', "this is a test  " + body);
+            });
+            index++
+        })
+    },5000);
 }
