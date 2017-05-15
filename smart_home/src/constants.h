2562889d371449ce8ee3e9d545b8c2f42478f58b
Vincze Dávid <vincdave@yahoo.com>
2017-05-15 20:26:39 +0300

    Added support for WeMoS D1

--- smart_home/src/constants.h
+++ smart_home/src/constants.h
@@ -9,15 +9,15 @@ const uint8_t FIRST_MUX_PIN = 16;//GPIO16 D0
 const uint8_t SECOND_MUX_PIN = 0;//GPIO5 D3
 const uint8_t THIRD_MUX_PIN = 2;//GPIO4 D4
 
-// const char BACKEND_SERVER_IP[] = "allamvizsga-akoszsebe.c9users.io";
-const char BACKEND_SERVER_IP[] = "192.168.0.25";
+const char BACKEND_SERVER_IP[] = "allamvizsga-akoszsebe.c9users.io";
+// const char BACKEND_SERVER_IP[] = "192.168.0.25";
 const char BACKEND_POST_API[] = "/saveRawData";
 const uint32_t BACKEND_SERVER_PORT = 8080;
 
-// const std::string WIFI_SSID = "Big_Cats";
-// const std::string WIFI_PASSWORD = "big cats gatekeeper";
-const std::string WIFI_SSID = "UPC6252419";
-const std::string WIFI_PASSWORD = "NUQMGGGB";
+const std::string WIFI_SSID = "Big_Cats";
+const std::string WIFI_PASSWORD = "big cats gatekeeper";
+// const std::string WIFI_SSID = "UPC6252419";
+// const std::string WIFI_PASSWORD = "NUQMGGGB";
 
 const uint8_t NUMBER_OF_SENSORS = 4;
 const uint32_t SAMPLING_TIME = 500;
