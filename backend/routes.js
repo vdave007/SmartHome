6fae24e5691c469cc911f7ae0ec22b822014362d
Vincze Dávid <vincdave@yahoo.com>
2017-06-03 20:13:55 +0300

    Added custom structure that keeps the last X number of measured current on the given cID

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,9 @@
 "use strict"
 let path = require('path')
+let FixedMultiQueue = require ('./fixedmultiqueue')
 
+var currentValues = [];
 
-var v1;
-var v2;
-var v3;
-var v4;
 
 module.exports = (app,dataBase) => {
 
@@ -21,14 +19,23 @@ module.exports = (app,dataBase) => {
 	})
 
 	app.get('/getamper', (req, res) => {
-			var house_id = req.param('house_id')
-	        console.log("Az utolso Amper",house_id)
-			if (typeof house_id === 'undefined') house_id = 1
-			house_id = parseInt(house_id)
-			var responseJson = [{"house_id":1,"ampervalue":v1*0.22,"amperdate":123},
-								{"house_id":1,"ampervalue":v2*0.22,"amperdate":123},
-								{"house_id":1,"ampervalue":v3*0.22,"amperdate":123},
-								{"house_id":1,"ampervalue":v4*0.22,"amperdate":123}]
+			var cid = req.param('cid')
+
+			if(currentValues[cid]==undefined)
+			{
+				res.send('INVALID CID!')
+				return
+			}
+
+			var latestData = currentValues[cid].getLatestDataObject()
+			console.log('Getting data',latestData)
+
+	        console.log("The latest measurment on device ",cid)
+						
+			var responseJson = [{"cid":cid,"ampervalue":parseInt(latestData[0])*0.22,"amperdate":123},
+								{"cid":cid,"ampervalue":parseInt(latestData[1])*0.22,"amperdate":123},
+								{"cid":cid,"ampervalue":parseInt(latestData[2])*0.22,"amperdate":123},
+								{"cid":cid,"ampervalue":parseInt(latestData[3])*0.22,"amperdate":123}]
 			// db.getAmper(house_id,function(returndata){
 				console.log("Response -----------------");
 				console.log(responseJson)
@@ -36,18 +43,22 @@ module.exports = (app,dataBase) => {
 			// })
 	})
 
-
-
 	app.post('/saveRawData', (req, res) => {
 		//res.json({info: "This is my backend!"})
 		console.log(req.body);
 		var cid = req.body.cid
 		var _date = new Date()
 		var date = _date.getTime()
-		v1 = req.body.v1
-		v2 = req.body.v2
-		v3 = req.body.v3
-		v4 = req.body.v4
+		if(currentValues[cid]==undefined)
+		{
+			currentValues[cid] = new FixedMultiQueue(4);
+		}
+		
+		var v1 = req.body.v1
+		var v2 = req.body.v2
+		var v3 = req.body.v3
+		var v4 = req.body.v4
+		currentValues[cid].pushInObjectInOrder([v1,v2,v3,v4])
 		console.log(cid,date,v1,v2,v3,v4)
 		dataBase.createRawInformation(cid,date,v1,v2,v3,v4,function(error){
 			if(error){
