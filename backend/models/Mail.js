0f2156433ed0e46ebdf70c36b6374978186e0f4d
Zsebe Akos <zsebea@yahoo.com>
2017-06-29 23:43:21 +0300

    email changed

--- backend/models/Mail.js
+++ backend/models/Mail.js
@@ -7,22 +7,22 @@ const Mail = module.exports = function () {
   this.transporter = nodemailer.createTransport({
     service: 'yahoo',
     auth: {
-      user: 'zsebea@yahoo.com',
-      pass: 'qwerty'
+      user: 'iotwithchem@yahoo.com',
+      pass: 'mehchtiwtoi2017'
     }
   });
   console.log('create mailer')
 };
 
 
-Mail.prototype.sendMail = function (from, message) {
+Mail.prototype.sendMail = function (to, reset_code) {
   console.log('here sending mail')
   // setup email data with unicode symbols
   let mailOptions = {
     from: 'zsebea@yahoo.com', // sender address
-    to: 'zsebea@yahoo.com', // list of receivers
-    subject: 'Reset code ' + from, // Subject line
-    text: message, // plain text body
+    to: to, // list of receivers
+    subject: 'Reset code ', // Subject line
+    text: reset_code, // plain text body
   };
 
   // send mail with defined transport object
@@ -31,7 +31,7 @@ Mail.prototype.sendMail = function (from, message) {
     if (error) {
       console.log(error);
     }
-    console.log('Message %s sent: %s', info.messageId, info.response);
+    //console.log('Message %s sent: %s', info.messageId, info.response);
   });
 
 };
