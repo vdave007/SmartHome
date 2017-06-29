b8559670c1e8ac5b348e7f3251baa2c9db409b4a
Zsebe Akos <zsebea@yahoo.com>
2017-06-29 23:10:11 +0300

    new Class for deciding devices identity

---
+++ backend/deviceselector.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var DeviceSelector = module.exports = function(dataBase){
+    this.init(dataBase)
+}
+
+DeviceSelector.prototype.init = function(dataBase){
+this.dataBase = dataBase
+}
+
+DeviceSelector.prototype.asyncGetActualDevicesWithSettings = function(responseJson,house_id,_callback){
+		this.dataBase.getDeviceSetting(house_id,function(returndatadevice){
+			if (returndatadevice != null){
+				var ds = []
+				responseJson.forEach(function(actualdevice) {
+				if (actualdevice.ampervalue != 0){	
+					var find = false;
+					returndatadevice.forEach(function(saveddevice) {
+						
+						if (saveddevice.value - saveddevice.valuedelay <= actualdevice.ampervalue 
+						&& saveddevice.value + saveddevice.valuedelay >= actualdevice.ampervalue){
+							find = true;
+							saveddevice.original_value = parseInt(saveddevice.value)
+							var data = {  
+								house_id : house_id,
+								name : saveddevice.name,
+								icon_id : saveddevice.icon_id,
+								value : parseInt(actualdevice.ampervalue),
+								original_value : parseInt(saveddevice.value),
+								valuedelay : saveddevice.valuedelay
+							}
+							ds.push(data)
+						}
+					},this)
+					if (!find)
+					{
+						var data = {  
+							house_id : house_id,
+							name : "Unknown",
+							icon_id : 0,
+							value : parseInt(actualdevice.ampervalue),
+							original_value : parseInt(actualdevice.ampervalue),
+							valuedelay : 1
+						}
+						ds.push(data)
+					}
+				}	
+			}, this);
+			return _callback(ds);
+			}	
+		})
+	}
