70519dc03fd170aceb1aa25014bf9b7ca24550b3
Zsebe Akos <zsebea@yahoo.com>
2017-06-29 23:08:53 +0300

    merged with development-Akos

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,14 @@
 "use strict"
 let mongoose = require('mongoose'),
 	path = require('path'),
-	RawData = require(path.resolve('./backend/models/rawdata.js'))
+	RawData = require(path.resolve('./backend/models/rawdata.js')),
+	Amper = require(path.resolve('./backend/models/Amper')),
+	DeviceSetting = require(path.resolve('./backend/models/DeviceSetting')),
+	House = require(path.resolve('./backend/models/House')),
+	User = require(path.resolve('./backend/models/User')),
+	UserHouse = require(path.resolve('./backend/models/UserHouse')),
+	ResetPassword = require(path.resolve('./backend/models/ResetPassword')),
+	UserSmartWatch = require(path.resolve('./backend/models/UserSmartWatch'))
 
 var DataBase = module.exports = function () {
 	this.mongoose = mongoose
@@ -20,14 +27,18 @@ DataBase.prototype.init = function () {
 		console.error('Could not connect to mongo server!', error)
 	})
 
-	var connectWithRetry = function() {
-	    return self.mongoose.connect('mongodb://localhost/smartHome', function(err) {
+	connectWithRetry(self) 
+}
+
+function connectWithRetry(self){
+		console.info('itt')
+	    //return self.mongoose.connect('mongodb://localhost/smartHome', function(err) {
+		return self.mongoose.connect('mongodb://heroku_xr2pj2sw:478kj7lelu6klbrklj3fbmnink@ds139869.mlab.com:39869/heroku_xr2pj2sw', function(err) {
 	        if (err) {
 	            console.error('Failed to connect to mongo on startup - retrying in 1 sec', err);
-	            setTimeout(connectWithRetry, 1000);
+	            setTimeout(connectWithRetry(self), 1000);
 	        }
 	    });
-	};
 }
 
 /**
@@ -63,4 +74,461 @@ DataBase.prototype.createRawInformation = function(cid,date,v1,v2,v3,v4,_callbac
 			return _callback(err)
 	})
 	return _callback(null)
+}
+
+/**
+ *  From Akos Db methods       ----      Amper need change to Watt 
+ */ 
+
+DataBase.prototype.getAmperInterval = function (house_id, datefrom, dateto, _callback) {
+
+	Amper.find({'amperdate': { '$gte': datefrom, '$lt': dateto }}, '-_id -__v', (error, ampers) => {
+		if (error) { return _callback(null) }
+		return _callback(ampers)
+	}).where('house_id').equals(house_id)
+}
+
+DataBase.prototype.getDeviceSetting = function (house_id, _callback) {
+
+	DeviceSetting.find({}, '-_id -__v', (error, ampers) => {
+		if (error) { return _callback(null) }
+		return _callback(ampers)
+	}).where('house_id').equals(house_id)
+}
+
+DataBase.prototype.createDeviceSettingMessage = function (hid,n,icid,v,vd,_callback){
+	var self = this
+
+	var t = new DeviceSetting({  
+		house_id : hid,
+		name : n,
+		icon_id : icid,
+		value : v,
+		valuedelay : vd
+	})
+	DeviceSetting.findOne({ house_id: hid, value: v }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			t.save(function(err) {
+					if (err) 
+						return _callback(err)
+			})
+		}
+		else
+		{
+			console.log(doc)
+			doc.name = n;
+			doc.icon_id = icid;
+			doc.valuedelay = vd;
+			doc.save();
+			console.log(doc)
+		}
+	});
+	
+	return _callback(null)
+}
+
+DataBase.prototype.deleteDeviceSettingMessage = function (hid,v,_callback){
+	var self = this
+	
+	DeviceSetting.findOne({house_id:hid, value: v }, function (err, doc){	
+		if (err || doc != null) 
+		{
+			doc.remove(function(err) {
+					if (err) 
+						return _callback(err)
+			})
+		}
+	});
+	
+	return _callback(null)
+}
+
+DataBase.prototype.getHouses = function (_callback) {
+
+	House.find({}, '-_id -__v', (error, houses) => {
+		if (error) { return _callback(null) }
+		return _callback(houses)
+	}) 
+}
+
+DataBase.prototype.getUserHouses = function (user_email,_callback) {
+
+	UserHouse.find({user_email : user_email}, '-_id -__v', (error, userhouses) => {
+		if (error) { return _callback(null) }
+		if (userhouses != null)
+		{
+			var ds = []
+			House.find({}, '-_id -__v', (error, houses) => {
+				if (houses != null)
+				{
+					userhouses.forEach(function(userhouse) {
+						houses.forEach(function(house){
+							if (userhouse.house_id == house.house_id)
+							{
+								ds.push(house)
+							}
+						})
+					})
+				}
+			return _callback(ds)	
+			})
+		}
+	}) 
+}
+
+DataBase.prototype.createHouseMessage = function (hname,hid,passwd,_callback){
+	var self = this
+	
+	House.findOne({"house_id":hid },'-_id -__v',(error, house) => {
+		if (house == null) 
+		{			
+			console.log("h---------------id : "+hid)
+			var t = new House({  
+				house_id : hid,
+				house_name : hname,
+				password : passwd
+			})
+			
+			t.save(function(err) {
+					if (err) 
+						return _callback(false)
+					return _callback(true)
+			})
+		}
+		else return _callback("van mar")
+	}).sort({ 'house_id': 'ascending' })
+}
+
+DataBase.prototype.updateHouseMessage = function (hid,hname,passwd,_callback){
+	var self = this
+	
+	var t = new House({  
+		house_id : hid,
+		house_name : hname,
+		password : passwd
+	})
+
+	House.findOne({ house_id: hid }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			t.save(function(err) {
+					if (err) 
+						return _callback(err)
+			})
+		}
+		else
+		{
+			console.log(doc)
+			doc.house_name = hname
+			doc.password = passwd
+			doc.save(function(err) {
+					if (err) 
+						return _callback(false)
+					return _callback(true)
+			})
+		}
+	});
+	
+	return _callback(null)
+}
+
+DataBase.prototype.updateHouseName = function (hname,hid,_callback){
+	var self = this
+	
+	House.findOne({"house_id":hid },(error, house) => {
+		if (error || house == null) 
+		{	
+		}
+		else
+		{
+			console.log("h---------------id : "+hid, house)
+			house.house_name = hname
+			house.save()
+			return _callback(true)
+		}
+	})
+}
+
+DataBase.prototype.getHousesByName = function (hname,_callback){
+	var self = this
+	
+	House.find({ "house_name": { "$regex": hname, "$options": "i" } }, '-_id -__v', (error, houses) => {
+		if (error) { return _callback(null) }
+		return _callback(houses)
+	})
+}
+
+DataBase.prototype.getHousesById = function (hid ,_callback){
+	var self = this
+	console.log("----",hid)
+	House.find( {"$where": "function() { return this.house_id.toString().match(/"+hid+"/) != null; }"}, '-_id -__v', (error, houses) => {
+		if (error) { return _callback(null) }
+		return _callback(houses)
+	})
+}
+
+DataBase.prototype.createUserMessage = function (u_email,passwd,_callback){
+	var self = this
+	
+	var t = new User({  
+		user_email : u_email,
+		password : passwd
+	})
+	
+	User.findOne({ user_email: u_email }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			t.save(function(err) {
+					if (err) 
+						return _callback(false)
+					return _callback(true)
+			})
+		}
+		else 
+		{
+			doc.password = passwd;
+			doc.save(function(err) {
+					if (err) 
+						return _callback(false)
+					return _callback(true)
+			})
+		}
+	});
+}
+
+DataBase.prototype.createUserMessage = function (u_email,passwd,number,_callback){
+	var self = this
+	
+	// create a Temperature json object 
+	var t = new User({  
+		user_email : u_email,
+		password : passwd,
+		phone_number : number
+	})
+	
+	User.findOne({ user_email: u_email }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			t.save(function(err) {
+					if (err) 
+						return _callback(false)
+					return _callback(true)
+			})
+		}
+		else 
+		{
+			doc.password = passwd;
+			doc.phone_number = number;
+			doc.save(function(err) {
+					if (err) 
+						return _callback(false)
+					return _callback(true)
+			})
+		}
+	});
+}
+
+DataBase.prototype.LoginUser = function (u_email,passwd,_callback){
+	var self = this
+	
+	User.findOne({ user_email: u_email,password : passwd }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			User.findOne({ user_email: u_email }, function (err, doc){
+				if (err || doc == null)
+				{
+					return _callback('false')
+				}
+				else 
+					return _callback('Wrong password')
+			});
+		}
+		else 
+		{
+			return _callback('true')
+		}
+	});
+}
+
+DataBase.prototype.UserPhoneNumber = function (u_email,_callback){
+	var self = this
+	
+	User.findOne({ user_email: u_email }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			return _callback(false)
+		}
+		else 
+		{
+			return _callback(doc.phone_number)
+		}
+	});
+}
+
+DataBase.prototype.LoginSmartWatch = function (smartwatch_id,_callback){
+	var self = this
+	
+	UserSmartWatch.findOne({ smartwatch_id: smartwatch_id }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			return _callback(false)
+		}
+		else 
+		{
+			return _callback(true)
+		}
+	});
+}
+
+DataBase.prototype.getSmartWatchUser = function (smartwatch_id,_callback){
+	var self = this
+	
+	UserSmartWatch.find({ smartwatch_id: smartwatch_id }, '-_id -__v -smartwatch_id', (error, users) => {
+		if (error) { return _callback(null) }
+		return _callback(users)
+	})
+}
+
+DataBase.prototype.getUserSmartWatch = function (user_email,_callback){
+	var self = this
+	
+	UserSmartWatch.find({ user_email: user_email }, '-_id -__v -user_email', (error, users) => {
+		if (error) { return _callback(null) }
+		return _callback(users)
+	})
+}
+
+DataBase.prototype.createUserHouseMessage = function (u_email,h_id,passwd, _callback){
+	var self = this
+	
+	var t = new UserHouse({  
+		user_email : u_email,
+		house_id : h_id
+	})
+	
+	User.findOne({user_email:u_email, password : passwd},function(err, doc) {
+	    if (err || doc == null)
+	    {
+	    	return _callback(err)
+	    }
+	    else
+	    {
+			UserHouse.findOne({ user_email: u_email,house_id : h_id }, function (err, doc){	
+				if (err || doc == null) 
+				{
+					t.save(function(err) {
+							if (err) 
+								return _callback(err)
+							return _callback(true)
+					})
+				}
+				else 
+				{
+					return _callback(false)
+				}
+			});	
+	    }
+	})
+	
+}
+
+DataBase.prototype.createUserSmartWatchMessage = function (user_email,smartwatch_id,_callback){
+	var self = this
+	
+	var t = new UserSmartWatch({  
+		user_email : user_email,
+		smartwatch_id : smartwatch_id
+	})
+	
+	User.findOne({user_email:user_email},function(err, doc) {
+	    if (err || doc == null)
+	    {
+	    	return _callback(err)
+	    }
+	    else
+	    {
+			UserSmartWatch.findOne({ user_email: user_email,smartwatch_id : smartwatch_id }, function (err, doc){	
+				if (err || doc == null) 
+				{
+					t.save(function(err) {
+							if (err) 
+								return _callback(err)
+							return _callback(true)
+					})
+				}
+				else 
+				{
+					return _callback(false)
+				}
+			});	
+	    }
+	})
+}
+
+DataBase.prototype.deleteUserHouse = function (u_email,h_id,_callback){
+	var self = this
+	
+	User.findOne({user_email:u_email},function(err, doc) {
+	    if (err || doc == null)
+	    {
+	    	return _callback(false)
+	    }
+	    else
+	    {
+	    	console.log('torol userhouse : ' +doc);
+			UserHouse.findOne({ user_email: u_email,house_id : h_id }, function (err, doc){	
+				if (err || doc == null) 
+				{
+					return _callback(false);
+				}
+				else 
+				{
+					doc.remove();
+					return _callback(true);
+				}
+			});	
+	    }
+	})
+	
+}
+
+DataBase.prototype.savePasswordReset = function (u_email,r_code,expire_time,_callback){
+	var self = this
+	
+	var t = new ResetPassword({  
+		user_email : u_email,
+		reset_code : r_code,
+		expire : expire_time
+	})
+	
+	ResetPassword.findOne({ user_email: u_email }, function (err, doc){	
+		if (err || doc == null) 
+		{
+			t.save(function(err) {
+					if (err) 
+						return _callback(err)
+					return _callback(true)
+			})
+		}
+		else 
+		{
+			doc.reset_code = r_code;
+			doc.expire = expire_time;
+			doc.save(function(err) {
+					if (err) 
+						return _callback(err)
+					return _callback(true)
+			})
+		}
+	});
+}
+
+DataBase.prototype.getResetPassword = function (user_email,_callback){
+	var self = this
+	
+	ResetPassword.findOne({ user_email: user_email }, '-_id -__v -user_email', (error, resetpasswd) => {
+		if (error) { return _callback(null) }
+		return _callback(resetpasswd)
+	})
 }
--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,8 @@
 "use strict"
-let path = require('path')
-
-
-module.exports = (app,dataBase,ai) => {
+let path = require('path'),
+	validator = require('validator')
+		
+module.exports = (app,dataBase,ai,encrypter,mail,sms,deviceselector) => {
 
 	app.get('/device/getConfigurationPage', (req,res) => {
 		res.sendFile(path.resolve('./backend/pages/configpage.html'))
@@ -71,11 +71,11 @@ module.exports = (app,dataBase,ai) => {
 
 		ai.addValues(cid,v1,v2,v3,v4)
 		console.log(cid,date,v1,v2,v3,v4)
-		dataBase.createRawInformation(cid,date,v1,v2,v3,v4,function(error){
-			if(error){
-				console.error('error:',error)
-			}
-		})
+		// dataBase.createRawInformation(cid,date,v1,v2,v3,v4,function(error){
+		// 	if(error){
+		// 		console.error('error:',error)
+		// 	}
+		// })
 		res.send("OK");
 	})
 
@@ -83,6 +83,354 @@ module.exports = (app,dataBase,ai) => {
 		res.send("SmartHomeServer")
 	})
 
+	/**
+	 *  routes for android devices 
+	 * 
+	 * 	- add to path /adroid/*   
+	 */
+	var v1 = 2*1000;
+	var v2 = 6.5*1000;
+	var v3 = 2.2*1000;
+	var v4 = 3.65*1000;
 
+	app.get('/getdeviceswithsettings', (req, res) => { ///getliveamper
+			var house_id = req.param('house_id')
+	        console.log("Az utolso Amper",house_id)
+			if (typeof house_id === 'undefined') house_id = "1"
+			var responseJson = [{"house_id":1,"ampervalue":v1*0.22,"amperdate":123},
+								{"house_id":1,"ampervalue":v2*0.22,"amperdate":123},
+								{"house_id":1,"ampervalue":v3*0.22,"amperdate":123},
+								{"house_id":1,"ampervalue":v4*0.22,"amperdate":123}]
+			deviceselector.asyncGetActualDevicesWithSettings(responseJson,house_id,function(datatouser){
+				res.json(datatouser)
+			})
+	})
 
+	app.get('/setdevicesetting', (req,res) => {
+	
+		console.log('feltolt setting');
+		
+		var house_id = req.param('house_id')
+		var name = req.param('name')
+		var icon_id = req.param('icon_id')
+		var value = req.param('value')
+		var valuedelay = req.param('valuedelay')
+		if (typeof house_id === 'undefined') return
+		if (typeof icon_id === 'undefined') return
+		if (typeof value === 'undefined') return
+		if (typeof valuedelay === 'undefined') return
+		icon_id = parseInt(icon_id)
+		value = parseInt(value)
+		valuedelay = parseInt(valuedelay)
+		console.log('frissit',valuedelay);
+		
+		dataBase.createDeviceSettingMessage(house_id,name,icon_id,value,valuedelay,function(err){
+			if (err) console.error(err) 
+			res.json('success');           
+		})
+	})
+
+	app.get('/deletedevicesetting', (req,res) => {
+	
+		console.log('feltolt setting');
+		
+		var house_id = req.param('house_id')
+		var value = req.param('value')
+		if (typeof house_id === 'undefined') return
+		if (typeof value === 'undefined') return
+		
+		value = parseInt(value)
+		
+		console.log("----torol ",house_id,value)
+		dataBase.deleteDeviceSettingMessage(house_id,value,function(err){
+			if (err) console.error(err) 
+			res.json('success');           
+		})
+	})
+
+	app.get('/getampers', (req, res) => {
+		var house_id = req.param('house_id')
+		var datefrom = req.param('date_from')
+		var dateto = req.param('date_to')
+		console.log("Az osszes amper ",datefrom,dateto)
+		if (typeof house_id === 'undefined') house_id = "1"
+
+		dataBase.getAmperInterval(house_id,datefrom,dateto,function(returndata){
+			res.json(returndata)
+		})
+
+	})
+
+	app.get('/getdevicesetting', (req, res) => {
+		var house_id = req.param('house_id')
+		console.log('devsett----')
+		if (typeof house_id === 'undefined') house_id = "1"
+
+		dataBase.getDeviceSetting(house_id,function(returndata){
+			res.json(returndata)
+		})
+	})
+
+	app.get('/gethouses', (req, res) => {
+        console.log("hAzak")
+       
+		dataBase.getHouses(function(returndata){
+			console.log(returndata)
+			res.json(returndata)
+		})
+	})
+
+	app.get('/getuserhouses', (req, res) => {
+        console.log("hAzak")
+        var user_email = req.param('user_email')
+		console.log('devsett----with settings')
+		if (typeof user_email === 'undefined') return
+		
+		dataBase.getUserHouses(user_email, function(returndata){
+		res.json(returndata)	
+		})
+	})
+
+	app.get('/gethousesbyname', (req, res) => {
+        console.log("hAzaklkj")
+		var house_name = req.param('house_name')
+		if (typeof house_name === 'undefined' || house_name == "") {res.json(null); return}
+		dataBase.getHousesByName(house_name, function(returndata){
+			 console.log(returndata)
+			res.json(returndata)
+		})
+	})
+
+	app.get('/gethousesbyid', (req, res) => {
+        console.log("hAzaklkj")
+		var house_id = req.param('house_id')
+		if (typeof house_id === 'undefined' || house_id == "") {res.json(null); return}
+		dataBase.getHousesById(house_id, function(returndata){
+			 console.log(returndata)
+			res.json(returndata)
+		})
+	})
+
+	app.get('/sethouse', (req,res) => {
+			
+		var house_name = req.param('house_name')
+		var house_id = req.param('house_id')
+		if (typeof house_name === 'undefined') house_name = ""	
+		if (typeof house_id === 'undefined' || house_id=="") { res.json(null); return }
+		console.log('feltolt house ' +house_name,house_id);
+
+		var passwd = encrypter.encrypt("vad")
+
+		dataBase.createHouseMessage(house_name,house_id,passwd,function(err){
+			res.json(err);           
+		})
+	})
+	
+	app.get('/updatehousename', (req,res) => {
+			
+		var house_name = req.param('house_name')
+		var house_id = req.param('house_id')
+		if (typeof house_name === 'undefined') return	
+		if (typeof house_id === 'undefined' || house_id=="") { res.json(null); return }
+		console.log('feltolt house ' +house_name,house_id);
+
+		dataBase.updateHouseName(house_name,house_id,function(err){
+			res.json(err);           
+		})
+	})
+
+	app.get('/setuser', (req,res) => {
+			
+		var user_email = req.param('user_email')
+		var password = req.param('password')
+		var number = req.param('number')
+		console.log('feltolt user : ' +user_email,password);
+		if (typeof user_email === 'undefined') return
+		if (typeof password === 'undefined') return
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		if (number === '')
+			dataBase.createUserMessage(user_email,encrypter.encrypt(password),function(err){
+				res.json(err)
+			})
+		else 
+			dataBase.createUserMessage(user_email,encrypter.encrypt(password),number,function(err){
+				res.json(err)
+			})
+	})
+
+	app.get('/login', (req,res) => {
+			
+		var user_email = req.param('user_email')
+		var password = req.param('password')
+		console.log('login : ' +user_email,password);
+		if (typeof user_email === 'undefined') {res.json('Email required'); return}
+		if (typeof password === 'undefined') { res.json('Password required'); return}
+
+		if (!validator.isEmail(user_email)) {res.json('Wrong email address'); return}
+
+		dataBase.LoginUser(user_email,encrypter.encrypt(password),function(isregisterd){
+			if (isregisterd !== 'undefined'	) 
+				res.json(isregisterd);           
+		})
+	})
+
+	app.get('/smartwatchlogin', (req,res) => {
+			
+		var smartwatch_id = req.param('smartwatch_id')
+
+		console.log('smartwatchlogin : ' +smartwatch_id);
+		if (typeof smartwatch_id === 'undefined') return
+
+		dataBase.LoginSmartWatch(smartwatch_id,function(isregisterd){
+			if (isregisterd !== 'undefined'	) 
+				res.json(isregisterd);           
+		})
+	})
+
+	app.get('/setuserhouse', (req,res) => {
+			
+		var user_email = req.param('user_email')
+		var house_id = req.param('house_id')
+		var passwd = req.param('password')
+		console.log('feltolt userhouse : ' +user_email , house_id);
+		if (typeof user_email === 'undefined') {res.json('Wrong email'); return}
+		if (typeof house_id === 'undefined') {res.json('Wrong id'); return}
+		if (typeof passwd === 'undefined') {res.json('Wrong password'); return}
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		
+		db.createUserHouseMessage(user_email,house_id,encrypter.encrypt(passwd),function(err){
+			res.json(err)
+		})
+	})
+	
+	app.get('/deleteuserhouse', (req,res) => {
+			
+		var user_email = req.param('user_email')
+		var house_id = req.param('house_id')
+		console.log('torol userhouse : ' +user_email , house_id);
+		if (typeof user_email === 'undefined') return
+		if (typeof house_id === 'undefined') return
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		
+		dataBase.deleteUserHouse(user_email,house_id,function(err){
+			res.json(err)
+		})
+	})
+
+	app.get('/setusersmartwatch', (req,res) => {
+			
+		var user_email = req.param('user_email')
+		var smartwatch_id = req.param('smartwatch_id')
+		console.log('feltolt userSmartWatch : ' +user_email,smartwatch_id);
+		if (typeof user_email === 'undefined') return
+		if (typeof smartwatch_id === 'undefined') return
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		
+		dataBase.createUserSmartWatchMessage(user_email,smartwatch_id,function(err){
+			res.json(err)
+		})
+	})
+
+	app.get('/getsmartwatchuser', (req, res) => {
+
+        var smartwatch_id = req.param('smartwatch_id')
+        console.log("smartwatchuser",smartwatch_id)
+		if (typeof smartwatch_id === 'undefined') return
+		
+		dataBase.getSmartWatchUser(smartwatch_id, function(returndata){
+		res.json(returndata)	
+		})
+	})
+	
+	app.get('/getusersmartwatch', (req, res) => {
+
+        var user_email = req.param('user_email')
+        console.log("usersmartwatches",user_email)
+		if (typeof user_email === 'undefined') return
+		
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		
+		dataBase.getUserSmartWatch(user_email, function(returndata){
+		res.json(returndata)	
+		})
+	})
+
+	app.get('/resetuserpassword', (req, res) => {
+
+        var user_email = req.param('user_email')
+        var inmail = req.param('inmail')
+        var reset_code = req.param('reset_code')
+        console.log("rest user password ",user_email)
+		if (typeof user_email === 'undefined') return
+		
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		
+		var reset_code = 'xxxxx'.replace(/[x]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			 return v.toString(16);
+		});
+		mail.sendMail(user_email,reset_code)
+		if (inmail === 'undefined' || inmail === "True")
+		{
+			mail.sendMail(user_email,reset_code)
+			console.log("mail",inmail)
+		}
+		else
+		{ 
+			dataBase.UserPhoneNumber(user_email,function(phone_number){
+				console.log("inmail ? ",inmail, phone_number)
+				if (phone_number != false)
+				if (phone_number != undefined)
+				{
+					if (phone_number.match("([0-9]{10})"))
+					{
+						sms.sendSms(phone_number,reset_code);
+					}
+					else
+					{	
+						mail.sendMail(user_email,reset_code)
+					}
+				}
+				else
+					{	
+						mail.sendMail(user_email,reset_code)
+					}
+			});
+		}
+		console.log('resetcode = ', reset_code)
+		dataBase.savePasswordReset(user_email,reset_code,new Date().getTime(),function(returndata){
+		res.json(returndata)
+		})	
+	})
+
+	app.get('/getresetuserpassword', (req, res) => {
+
+        var user_email = req.param('user_email')
+        var reset_code = req.param('reset_code')
+        console.log("get rest user password ",user_email)
+		if (typeof user_email === 'undefined') return
+		
+		if (!validator.isEmail(user_email)) {res.json('hakker'); return}
+		
+		dataBase.getResetPassword(user_email,function(returndata){
+		if (returndata != null)
+		{
+			console.log(returndata.reset_code,returndata.expire);
+			if ((returndata.expire + 60000*5) > new Date().getTime())
+			{
+					console.log("not expired");
+					if (returndata.reset_code == reset_code)
+					{
+						console.log("reset_codes match");
+						res.json(true);
+						return;
+					}
+			}
+			res.json(false);
+			return;
+		}
+		else
+			res.json(false);
+		})
+	})
 }
