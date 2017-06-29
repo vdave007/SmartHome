35238a81ee1858f67b0e47603fda1f70028096d4
Zsebe Akos <zsebea@yahoo.com>
2017-06-29 23:11:26 +0300

    added new dependencies for Encryptation , Mail, Sms, DeviceSelector

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ let express = require('express'),
 	bodyParser = require('body-parser'),
  	app = express(),
  	DataBase = require('./backend/database'),
-	CurrentAI = require('./backend/currentai')
+	CurrentAI = require('./backend/currentai'),
+	Encrypter = require('./backend/models/Encrypter'),
+	Mail = require('./backend/models/Mail'),
+	Sms = require('./backend/models/Sms'),
+	DeviceSelector = require('./backend/deviceselector')
+
+
 
 app.set('port', process.env.PORT || 8080)
 
@@ -24,4 +30,12 @@ var ai = new CurrentAI()
 
 var dataBase = new DataBase()
 
-require('./backend/routes')(app,dataBase,ai)
+var encrypter = new Encrypter();
+
+var mail = new Mail();
+
+var sms = new Sms();
+
+var deviceselector = new DeviceSelector(dataBase);
+
+require('./backend/routes')(app,dataBase,ai,encrypter,mail,sms,deviceselector)
